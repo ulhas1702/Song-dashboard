81af4d20d648a703115bc6e00159596a9e80cd86
ulhas1702 <ulhaskalasa1604@gmail.com>
2024-04-23 11:53:48 +0530

    progress-bar created

--- src/player/audios.js
+++ src/player/audios.js
@@ -1,7 +1,8 @@
 exports.songsdata = [
     {
         "title":"Darke - Forever",
-        "url":"https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba-online-audio-converter.com_-1.wav"
+        "url":"https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba-online-audio-converter.com_-1.wav",
+        "duration": 348
     },
     {
         "title":"Linking Park - In the end",
--- src/player/player.js
+++ src/player/player.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './player.css'
 import { FaArrowRotateLeft } from "react-icons/fa6";
 import { FaArrowRotateRight } from "react-icons/fa6";
@@ -7,8 +7,8 @@ import { FaPause } from "react-icons/fa6";
 import { IoPlayBack } from "react-icons/io5";
 import { IoPlayForward } from "react-icons/io5";
 
-const Player = ({audioElem, isplay, setIsplay, currentsong, setCurrentsong, songs}) => {
-
+const Player = ({audioPlayer, audioElem, curAudioPos, isplay, setIsplay, currentsong, setCurrentsong, songs, handleAudioSeek}) => {
+    const [audioLen, setAudioLen] = useState(0);
     const playpause = () => {
          setIsplay(!isplay)
     }
@@ -36,14 +36,41 @@ const Player = ({audioElem, isplay, setIsplay, currentsong, setCurrentsong, song
         audioElem.current.currentTime = 0
     }
 
+    useEffect(() => {
+        let audioLengh = 0;
+        const audioEle = document.getElementById("audioPlayer");
+        if(audioEle) {
+            if(audioEle.duration == 'Infinity') {
+                audioEle.load();
+                audioEle.currentTime = 24*60*60; //fake big time
+                audioEle.volume = 0;
+            }
+            audioLengh = audioEle.duration;
+        }
+        setAudioLen(audioLengh);
+        console.log("audioLen", audioLengh)
+    }, [audioElem])
+
+    const backward = () => {
+        audioElem.current.currentTime -= 10;
+    }
+    
+    const forward = () => {
+        audioElem.current.currentTime += 10;
+    }
+
   return (
     <div className='center'>
+        <p>{currentsong.img}</p>
         <p>{currentsong.title}</p>
-        <button className='back'>10 <FaArrowRotateLeft/></button>
+        <button className='back' onClick={backward}>10 <FaArrowRotateLeft/></button>
         <button className='playpause' onClick={playpause}>
             {isplay ? <FaPause/> : <FaPlay/>}
         </button>
-        <button className='forward'>10 <FaArrowRotateRight/></button>
+        <button className='forward' onClick={forward}>10 <FaArrowRotateRight/></button>
+        <div>
+            <input style={{width:"250px"}} type="range" value={curAudioPos} min={0} max={348} onChange={(e) => handleAudioSeek(e.target.value)}></input>
+        </div>
         <div>
             <button className='playpause1'><IoPlayBack onClick={skipback}/></button>
             <button className='playpause2'><IoPlayForward onClick={skipNext}/></button>
